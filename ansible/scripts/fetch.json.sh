cb4bff72f2882f08ad5363eef2171aecbf59dd36
jacob-jarick <mem.namefix@gmail.com>
2023-04-26 20:19:36 +0800

    HASS stuff

--- ansible/scripts/fetch.json.sh
+++ ansible/scripts/fetch.json.sh
@@ -10,3 +10,7 @@ curl -s http://10.1.1.227/json_cells > /var/www/html/esp32_json/office.cells.jso
 
 curl -s http://10.1.1.212/jsonapi > /var/www/html/esp32_json/network.api.json
 curl -s http://10.1.1.212/json_cells > /var/www/html/esp32_json/network.cells.json
+
+
+# lounge
+curl -s http://10.1.1.191/jsonapi > /var/www/html/esp32_json/lounge.api.json
